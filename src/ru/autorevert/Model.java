43c3a9d8eba5289f8a2a365e4ea03106add98f37
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-10 20:25:23 +0100

    rewriting Model in java (3)

--- src/ru/autorevert/Model.java
+++ src/ru/autorevert/Model.java
@@ -2,14 +2,16 @@ package ru.autorevert;
 
 import com.intellij.openapi.project.Project;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * User: dima
  * Date: 10/06/2012
  */
 public class Model {
 	private boolean started = false;
-
 	private final Project project;
+	private int counter;
 
 	public Model(Project project) {
 		this.project = project;
@@ -17,9 +19,36 @@ public class Model {
 
 	public synchronized void start() {
 		started = true;
+		counter = 0;
 	}
 
 	public synchronized void stop() {
 		started = false;
+	}
+
+	public synchronized void onTimer() {
+		if (!started) return;
+
+		counter++;
+		showPopup(project, "" + counter);
+
+		if (counter % TimeUnit.MINUTES.toSeconds(2) == 0) {
+			revertChanges();
+			counter = 0;
+		}
+	}
+
+	public synchronized void onCommit() {
+		counter = 0;
+	}
+
+	private void revertChanges() {
+		// TODO implement
+
+	}
+
+	private void showPopup(Project project, String message) {
+		// TODO implement
+
 	}
 }
