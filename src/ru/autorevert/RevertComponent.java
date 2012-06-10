1e61fc95d5bccc83081ef5a48822eb7d0b699a28
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-10 20:37:39 +0100

    rewriting RevertComponent in java (1)

--- src/ru/autorevert/RevertComponent.java
+++ src/ru/autorevert/RevertComponent.java
@@ -3,6 +3,11 @@ package ru.autorevert;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.AbstractVcs;
+import com.intellij.openapi.vcs.checkin.BaseCheckinHandlerFactory;
+import com.intellij.openapi.vcs.impl.CheckinHandlersManager;
+
+import java.util.List;
 
 /**
  * User: dima
@@ -10,23 +15,26 @@ import com.intellij.openapi.project.Project;
  */
 public class RevertComponent extends AbstractProjectComponent {
 	private final Model model;
-	private final TimerEventsSource.Listener listener;
+	private TimerEventsSource.Listener listener;
 
 	protected RevertComponent(Project project) {
 		super(project);
 		model = new Model(project);
-		listener = new TimerEventsSource.Listener() {
-			@Override public void onTimerEvent() {
-
-			}
-		};
 	}
 
 	@Override public void initComponent() {
 		super.initComponent();
 
 		TimerEventsSource timerEventsSource = ApplicationManager.getApplication().getComponent(TimerEventsSource.class);
+		listener = new TimerEventsSource.Listener() {
+			@Override public void onTimerEvent() {
+				model.onTimer();
+			}
+		};
 		timerEventsSource.addListener(listener);
+
+		// register commit callback
+		List<BaseCheckinHandlerFactory> factories = CheckinHandlersManager.getInstance().getRegisteredCheckinHandlerFactories(new AbstractVcs[]{});
 
 	}
 
