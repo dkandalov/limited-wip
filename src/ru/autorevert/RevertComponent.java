ebab25a41f5583738e3c1e7dd329782e2fc2c55b
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-10 20:35:10 +0100

    rewriting RevertComponent in java

--- src/ru/autorevert/RevertComponent.java
+++ src/ru/autorevert/RevertComponent.java
@@ -1,5 +1,6 @@
 package ru.autorevert;
 
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.project.Project;
 
@@ -8,8 +9,28 @@ import com.intellij.openapi.project.Project;
  * Date: 10/06/2012
  */
 public class RevertComponent extends AbstractProjectComponent {
+	private final Model model;
+	private final TimerEventsSource.Listener listener;
 
 	protected RevertComponent(Project project) {
 		super(project);
+		model = new Model(project);
+		listener = new TimerEventsSource.Listener() {
+			@Override public void onTimerEvent() {
+
+			}
+		};
+	}
+
+	@Override public void initComponent() {
+		super.initComponent();
+
+		TimerEventsSource timerEventsSource = ApplicationManager.getApplication().getComponent(TimerEventsSource.class);
+		timerEventsSource.addListener(listener);
+
+	}
+
+	@Override public void disposeComponent() {
+		super.disposeComponent();
 	}
 }
