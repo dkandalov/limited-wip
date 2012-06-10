611a4553b14d842ee9967c11cc6a7b95a8840503
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-10 23:23:31 +0100

    improved start/stop action

--- src/ru/autorevert/StartOrStopAutoRevert.java
+++ src/ru/autorevert/StartOrStopAutoRevert.java
@@ -9,26 +9,6 @@ import com.intellij.openapi.project.Project;
  * Date: 10/06/2012
  */
 public class StartOrStopAutoRevert extends AnAction {
-	@Override public void update(AnActionEvent event) {
-		String text = textFor(event.getProject());
-		event.getPresentation().setText(text);
-		event.getPresentation().setDescription(text);
-		event.getPresentation().setEnabled(event.getProject() != null);
-	}
-
-	private String textFor(Project project) {
-		if (project == null) return "Start auto-revert";
-
-		Model model = project.getComponent(RevertComponent.class).getModel();
-		String text;
-		if (model.isStarted()) {
-			text = "Stop auto-revert";
-		} else {
-			text = "Start auto-revert";
-		}
-		return text;
-	}
-
 	@Override public void actionPerformed(AnActionEvent event) {
 		Project project = event.getProject();
 		if (project == null) return;
@@ -38,6 +18,24 @@ public class StartOrStopAutoRevert extends AnAction {
 			model.stop();
 		} else {
 			model.start();
+		}
+	}
+
+	@Override public void update(AnActionEvent event) {
+		String text = textFor(event.getProject());
+		event.getPresentation().setText(text);
+		event.getPresentation().setDescription(text);
+		event.getPresentation().setEnabled(event.getProject() != null);
+	}
+
+	private static String textFor(Project project) {
+		if (project == null) return "Start auto-revert";
+
+		Model model = project.getComponent(RevertComponent.class).getModel();
+		if (model.isStarted()) {
+			return "Stop auto-revert";
+		} else {
+			return "Start auto-revert";
 		}
 	}
 }
