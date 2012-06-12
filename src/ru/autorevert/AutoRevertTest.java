d18030d353564d22cc7d132d938d492b5a9cad56
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-12 20:39:30 +0100

    test for core logic 1

--- src/ru/autorevert/AutoRevertTest.java
+++ src/ru/autorevert/AutoRevertTest.java
@@ -8,11 +8,26 @@ import org.junit.Test;
  */
 public class AutoRevertTest {
 
-	@Test public void aaa() {
-		new Model();
+	@Test public void whenStartedModelShouldSendNotificationToUI() {
+
+		IDEService ideService = new IDEService();
+		Model model = new Model(ideService);
+		model.start();
 	}
 
 	private static class Model {
 
+		public Model(IDEService ideService) {
+			// TODO implement
+
+		}
+
+		public void start() {
+			// TODO implement
+
+		}
+	}
+
+	private static class IDEService {
 	}
 }
