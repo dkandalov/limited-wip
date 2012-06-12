c626e9f1e2516d986b23513af4aad9e07f376094
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2012-06-12 21:42:31 +0100

    pulling out IDE classes

--- src/ru/autorevert/IdeActions.java
+++ src/ru/autorevert/IdeActions.java
@@ -21,15 +21,20 @@ public class IdeActions {
 		this.project = project;
 	}
 
-	public void revertCurrentChangeList() {
+	public boolean revertCurrentChangeList() {
 		try {
+			final boolean[] result = new boolean[]{true};
+
 			SwingUtilities.invokeAndWait(new Runnable() {
 				@Override public void run() {
 					// TODO probably should detect commit dialog being open and don't revert in this case
 					// TODO still get errors when commit overlaps auto-revert
 					// TODO still get "reload from file system" popup questions
 					LocalChangeList changeList = ChangeListManager.getInstance(project).getDefaultChangeList();
-					if (changeList.getChanges().isEmpty()) return;
+					if (changeList.getChanges().isEmpty()) {
+						result[0] = false;
+						return;
+					}
 
 					new RollbackWorker(project, true).doRollback(changeList.getChanges(), true, null, null);
 					for (Change change : changeList.getChanges()) {
@@ -39,6 +44,8 @@ public class IdeActions {
 //					showPopup(project, "!" + changeList.getName() + " " + changeList.getChanges()); // TODO
 				}
 			});
+
+			return result[0];
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		} catch (InvocationTargetException e) {
