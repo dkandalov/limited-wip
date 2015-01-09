caaae627e28773f4fe9b45d85377da0e6f60b7f4
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2015-01-09 21:05:10 +0400

    extracted VcsUtil

--- src/limitedwip/components/DisableCommitsWithErrorsComponent.java
+++ src/limitedwip/components/DisableCommitsWithErrorsComponent.java
@@ -15,75 +15,58 @@ package limitedwip.components;
 
 import com.intellij.notification.*;
 import com.intellij.openapi.components.ApplicationComponent;
-import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.options.ShowSettingsUtil;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Ref;
-import com.intellij.openapi.vcs.CheckinProjectPanel;
-import com.intellij.openapi.vcs.VcsKey;
-import com.intellij.openapi.vcs.changes.CommitExecutor;
-import com.intellij.openapi.vcs.checkin.BeforeCheckinDialogHandler;
-import com.intellij.openapi.vcs.checkin.CheckinHandler;
-import com.intellij.openapi.vcs.checkin.VcsCheckinHandlerFactory;
-import com.intellij.openapi.vcs.impl.CheckinHandlersManager;
+import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.problems.WolfTheProblemSolver;
-import com.intellij.util.Function;
-import com.intellij.util.containers.MultiMap;
+import limitedwip.components.VcsUtil.CheckinListener;
 import limitedwip.ui.settings.Settings;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.event.HyperlinkEvent;
-import java.lang.reflect.Field;
 import java.util.List;
 
-import static java.util.Arrays.asList;
+import static limitedwip.components.VcsUtil.registerBeforeCheckInListener;
 
 public class DisableCommitsWithErrorsComponent implements ApplicationComponent, Settings.Listener {
-	private static final Logger LOG = Logger.getInstance(DisableCommitsWithErrorsComponent.class);
-
 	private final Ref<Boolean> enabled = Ref.create(false);
 
-	@SuppressWarnings("unchecked")
 	@Override public void initComponent() {
+		registerBeforeCheckInListener(new CheckinListener() {
+			@Override public boolean allowCheckIn(@NotNull Project project, @NotNull List<Change> changes) {
+				if (!enabled.get()) return true;
 
-		// This is a hack caused by limitations of IntelliJ API.
-		//  - cannot use CheckinHandlerFactory because its createSystemReadyHandler() is never called
-		//  - cannot use VcsCheckinHandlerFactory through extension points because need to register
-		//    checkin handler for all VCSs available
-		//  - cannot use CheckinHandlersManager#registerCheckinHandlerFactory() because it doesn't properly
-		//    register VcsCheckinHandlerFactory
-		//
-		// Therefore, this hack with reflection.
-
-		accessField(CheckinHandlersManager.getInstance(), asList("b", "myVcsMap"), new Function<MultiMap, Void>() {
-			@Override public Void fun(MultiMap multiMap) {
-
-				for (Object key : multiMap.keySet()) {
-					multiMap.putValue(key, new ProhibitingCheckinHandlerFactory((VcsKey) key, enabled));
+				WolfTheProblemSolver wolf = WolfTheProblemSolver.getInstance(project);
+				for (Module module : ModuleManager.getInstance(project).getModules()) {
+					if (wolf.hasProblemFilesBeneath(module)) {
+						notifyThatCommitWasCancelled();
+						return false;
+					}
 				}
-
-				return null;
+				return true;
 			}
 		});
 	}
 
-	@SuppressWarnings("unchecked")
-	private static void accessField(Object o, List<String> possibleFieldNames, Function function) {
-		for (Field field : o.getClass().getDeclaredFields()) {
-			if (possibleFieldNames.contains(field.getName())) {
-				field.setAccessible(true);
-				try {
-
-					function.fun(field.get(o));
-					return;
-
-				} catch (Exception e) {
-					LOG.warn("Error while initializing ProhibitingCheckinHandlerFactory");
-				}
+	private void notifyThatCommitWasCancelled() {
+		NotificationListener listener = new NotificationListener() {
+			@Override
+			public void hyperlinkUpdate(@NotNull Notification notification, @NotNull HyperlinkEvent event) {
+				ShowSettingsUtil.getInstance().showSettingsDialog(null, LimitedWIPAppComponent.class);
 			}
-		}
+		};
+
+		Notifications notificationsManager = (Notifications) NotificationsManager.getNotificationsManager();
+		notificationsManager.notify(new Notification(
+				LimitedWIPAppComponent.displayName,
+				"You cannot commit because project has errors",
+				"(It can be turned off in <a href=\"\">Settings</a> dialog)",
+				NotificationType.WARNING,
+				listener
+		));
 	}
 
 	@Override public void disposeComponent() {
@@ -96,57 +79,4 @@ public class DisableCommitsWithErrorsComponent implements ApplicationComponent, 
 	@Override public void onSettings(Settings settings) {
 		enabled.set(settings.disableCommitsWithErrors);
 	}
-
-	private static class ProhibitingCheckinHandlerFactory extends VcsCheckinHandlerFactory {
-		private final Ref<Boolean> enabled;
-
-		protected ProhibitingCheckinHandlerFactory(@NotNull VcsKey key, Ref<Boolean> enabled) {
-			super(key);
-			this.enabled = enabled;
-		}
-
-		@Override public BeforeCheckinDialogHandler createSystemReadyHandler(@NotNull final Project project) {
-			return new BeforeCheckinDialogHandler() {
-				@Override
-				public boolean beforeCommitDialogShownCallback(Iterable<CommitExecutor> commitExecutors, boolean b) {
-					if (!enabled.get()) return true;
-
-					WolfTheProblemSolver wolf = WolfTheProblemSolver.getInstance(project);
-					for (Module module : ModuleManager.getInstance(project).getModules()) {
-						if (wolf.hasProblemFilesBeneath(module)) {
-							notifyThatCommitWasCancelled();
-							return false;
-						}
-					}
-					return true;
-				}
-			};
-		}
-
-		private void notifyThatCommitWasCancelled() {
-			NotificationListener listener = new NotificationListener() {
-				@Override
-				public void hyperlinkUpdate(@NotNull Notification notification, @NotNull HyperlinkEvent event) {
-					ShowSettingsUtil.getInstance().showSettingsDialog(null, LimitedWIPAppComponent.class);
-				}
-			};
-
-			Notifications notificationsManager = (Notifications) NotificationsManager.getNotificationsManager();
-			notificationsManager.notify(new Notification(
-					LimitedWIPAppComponent.displayName,
-					"You cannot commit because project has errors",
-					"(It can be turned off in <a href=\"\">Settings</a> dialog)",
-					NotificationType.WARNING,
-					listener
-			));
-		}
-
-		@NotNull @Override protected CheckinHandler createVcsHandler(CheckinProjectPanel panel) {
-			return new MyDummyHandler();
-		}
-
-		private static class MyDummyHandler extends CheckinHandler {}
-
-	}
-
 }
---
+++ src/limitedwip/components/VcsUtil.java
@@ -0,0 +1,94 @@
+package limitedwip.components;
+
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.CheckinProjectPanel;
+import com.intellij.openapi.vcs.VcsKey;
+import com.intellij.openapi.vcs.changes.Change;
+import com.intellij.openapi.vcs.changes.CommitExecutor;
+import com.intellij.openapi.vcs.checkin.BeforeCheckinDialogHandler;
+import com.intellij.openapi.vcs.checkin.CheckinHandler;
+import com.intellij.openapi.vcs.checkin.VcsCheckinHandlerFactory;
+import com.intellij.openapi.vcs.impl.CheckinHandlersManager;
+import com.intellij.util.Function;
+import com.intellij.util.containers.MultiMap;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+class VcsUtil {
+    private static final Logger LOG = Logger.getInstance(VcsUtil.class);
+
+    @SuppressWarnings("unchecked")
+    public static void registerBeforeCheckInListener(final CheckinListener listener) {
+        // This is a hack caused by limitations of IntelliJ API.
+        //  - cannot use CheckinHandlerFactory because:
+        //		- CheckinHandler is used just before commit (and after displaying commit dialog)
+        // 		- its CheckinHandlerFactory#createSystemReadyHandler() doesn't seem to be called
+        //  - cannot use VcsCheckinHandlerFactory through extension points because need to register
+        //    checkin handler for all VCSs available
+        //  - cannot use CheckinHandlersManager#registerCheckinHandlerFactory() because it doesn't properly
+        //    register VcsCheckinHandlerFactory
+        //
+        // Therefore, using reflection.
+
+        accessField(CheckinHandlersManager.getInstance(), asList("b", "myVcsMap"), new Function<MultiMap, Void>() {
+            @Override public Void fun(MultiMap multiMap) {
+
+                for (Object key : multiMap.keySet()) {
+                    multiMap.putValue(key, new DelegatingCheckinHandlerFactory((VcsKey) key, listener));
+                }
+
+                return null;
+            }
+        });
+    }
+
+    @SuppressWarnings("unchecked")
+    private static void accessField(Object object, List<String> possibleFieldNames, Function function) {
+        for (Field field : object.getClass().getDeclaredFields()) {
+            if (possibleFieldNames.contains(field.getName())) {
+                field.setAccessible(true);
+                try {
+
+                    function.fun(field.get(object));
+                    return;
+
+                } catch (Exception ignored) {
+                }
+            }
+        }
+        LOG.warn("Failed to access fields: " + possibleFieldNames + " on '" + object + "'");
+    }
+
+
+    public static interface CheckinListener {
+        boolean allowCheckIn(@NotNull Project project, @NotNull List<Change> changes);
+    }
+
+
+    private static class DelegatingCheckinHandlerFactory extends VcsCheckinHandlerFactory {
+        private final CheckinListener listener;
+
+        protected DelegatingCheckinHandlerFactory(@NotNull VcsKey key, CheckinListener listener) {
+            super(key);
+            this.listener = listener;
+        }
+
+        @Override public BeforeCheckinDialogHandler createSystemReadyHandler(@NotNull final Project project) {
+            return new BeforeCheckinDialogHandler() {
+                @Override public boolean beforeCommitDialogShown(@NotNull Project project, @NotNull List<Change> changes,
+                                                                 @NotNull Iterable<CommitExecutor> executors, boolean showVcsCommit) {
+                    return listener.allowCheckIn(project, changes);
+                }
+            };
+        }
+
+        @NotNull @Override protected CheckinHandler createVcsHandler(CheckinProjectPanel panel) {
+            return new CheckinHandler() {};
+        }
+    }
+}
