48728651757ddfcdc21bf6d1f8070585aa4ec17d
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2016-03-25 10:37:45 +0000

    calculate change size in background

--- src/limitedwip/watchdog/components/ChangeSizeCalculator.java
+++ src/limitedwip/watchdog/components/ChangeSizeCalculator.java
@@ -12,36 +12,37 @@ import com.intellij.openapi.editor.event.DocumentListener;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.util.Computable;
+import com.intellij.openapi.util.Pair;
 import com.intellij.openapi.vcs.VcsException;
 import com.intellij.openapi.vcs.changes.*;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.diff.FilesTooBigForDiffException;
+import limitedwip.common.PluginId;
 import limitedwip.watchdog.ChangeSize;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static com.intellij.openapi.diff.impl.util.TextDiffTypeEnum.*;
 
 public class ChangeSizeCalculator {
-	private static final long diffDurationThresholdMillis = 200;
 	private final ChangeSizeCache changeSizeCache;
 	private final Project project;
 	private ChangeSize changeSize;
+	private volatile boolean isRunningBackgroundDiff;
 
 
 	public ChangeSizeCalculator(Project project) {
 		this.project = project;
+		this.changeSize = new ChangeSize(0, true);
 		this.changeSizeCache = new ChangeSizeCache();
 	}
 
 	public void onTimer() {
-		changeSize = ApplicationManager.getApplication().runReadAction(new Computable<ChangeSize>() {
-			@Override public ChangeSize compute() {
-				return calculateCurrentChangeListSizeInLines();
-			}
-		});
+		calculateCurrentChangeListSizeInLines();
 	}
 
 	public ChangeSize currentChangeListSizeInLines() {
@@ -51,35 +52,63 @@ public class ChangeSizeCalculator {
 	/**
 	 * Can't use com.intellij.openapi.vcs.impl.LineStatusTrackerManager here because it only tracks changes for open files.
 	 */
-	private ChangeSize calculateCurrentChangeListSizeInLines() {
-		long startTime = System.currentTimeMillis();
-		TextCompareProcessor compareProcessor = new TextCompareProcessor(
-				ComparisonPolicy.TRIM_SPACE,
-				DiffPolicy.LINES_WO_FORMATTING,
-				HighlightMode.BY_LINE
-		);
-		LocalChangeList changeList = ChangeListManager.getInstance(project).getDefaultChangeList();
+	private void calculateCurrentChangeListSizeInLines() {
+		if (isRunningBackgroundDiff) return;
 
-		ChangeSize result = new ChangeSize(0);
-		for (Change change : changeList.getChanges()) {
-			Document document = getDocumentFor(change);
+		final Pair<ChangeSize, List<Change>> pair = ApplicationManager.getApplication().runReadAction(new Computable<Pair<ChangeSize, List<Change>>>() {
+			@Override public Pair<ChangeSize, List<Change>> compute() {
+				LocalChangeList changeList = ChangeListManager.getInstance(project).getDefaultChangeList();
+				List<Change> changesToDiff = new ArrayList<Change>();
 
-			long duration = System.currentTimeMillis() - startTime;
-			if (duration > diffDurationThresholdMillis) {
-				result = new ChangeSize(result.value, true);
-				break;
-			}
-
-			ChangeSize changeSize = changeSizeCache.get(document);
-			if (changeSize == null) {
-				changeSize = currentChangeListSizeInLines(change, startTime, compareProcessor);
-				if (!changeSize.isApproximate) {
-					changeSizeCache.put(document, changeSize);
+				ChangeSize result = new ChangeSize(0);
+				for (Change change : changeList.getChanges()) {
+					Document document = getDocumentFor(change);
+					ChangeSize changeSize = changeSizeCache.get(document);
+					if (changeSize == null) {
+						changesToDiff.add(change);
+					} else {
+						result = result.add(changeSize);
+					}
 				}
+				return Pair.create(result, changesToDiff);
 			}
-			result = result.add(changeSize);
+		});
+		if (pair.second.isEmpty()) {
+			changeSize = pair.first;
+			return;
 		}
-		return result;
+
+		new Thread(new Runnable() {
+			@Override public void run() {
+				isRunningBackgroundDiff = true;
+
+				TextCompareProcessor compareProcessor = new TextCompareProcessor(
+						ComparisonPolicy.TRIM_SPACE,
+						DiffPolicy.LINES_WO_FORMATTING,
+						HighlightMode.BY_LINE
+				);
+				final Map<Change, ChangeSize> changeSizeByChange = new HashMap<Change, ChangeSize>();
+				for (Change change : pair.second) {
+					changeSizeByChange.put(change, currentChangeListSizeInLines(change, compareProcessor));
+				}
+
+				ApplicationManager.getApplication().invokeLater(new Runnable() {
+					@Override public void run() {
+						changeSize = pair.first;
+						for (ChangeSize it : changeSizeByChange.values()) {
+							changeSize = changeSize.add(it);
+						}
+						for (Map.Entry<Change, ChangeSize> entry : changeSizeByChange.entrySet()) {
+							Document document = getDocumentFor(entry.getKey());
+							if (document != null && !entry.getValue().isApproximate) {
+								changeSizeCache.put(document, entry.getValue());
+							}
+						}
+						isRunningBackgroundDiff = false;
+					}
+				});
+			}
+		}, PluginId.value + "-DiffThread").start();
 	}
 
 	@Nullable private static Document getDocumentFor(Change change) {
@@ -87,10 +116,10 @@ public class ChangeSizeCalculator {
 		return virtualFile == null ? null : FileDocumentManager.getInstance().getDocument(virtualFile);
 	}
 
-	private static ChangeSize currentChangeListSizeInLines(Change change, long startTime, TextCompareProcessor compareProcessor) {
+	private static ChangeSize currentChangeListSizeInLines(Change change, TextCompareProcessor compareProcessor) {
 		try {
 
-			return amountOfChangedLinesIn(change, compareProcessor, startTime);
+			return amountOfChangedLinesIn(change, compareProcessor);
 
 		} catch (VcsException ignored) {
 			return new ChangeSize(0, true);
@@ -99,7 +128,7 @@ public class ChangeSizeCalculator {
 		}
 	}
 
-	private static ChangeSize amountOfChangedLinesIn(Change change, TextCompareProcessor compareProcessor, long startTime) throws VcsException, FilesTooBigForDiffException {
+	private static ChangeSize amountOfChangedLinesIn(Change change, TextCompareProcessor compareProcessor) throws VcsException, FilesTooBigForDiffException {
 		ContentRevision beforeRevision = change.getBeforeRevision();
 		ContentRevision afterRevision = change.getAfterRevision();
 		if (beforeRevision instanceof FakeRevision || afterRevision instanceof FakeRevision) {
@@ -121,10 +150,6 @@ public class ChangeSizeCalculator {
 				result += fragment.getModifiedLines1();
 			} else if (fragment.getType() == CHANGED || fragment.getType() == CONFLICT || fragment.getType() == INSERT) {
 				result += fragment.getModifiedLines2();
-			}
-			long duration = System.currentTimeMillis() - startTime;
-			if (duration > diffDurationThresholdMillis) {
-				return new ChangeSize(result, true);
 			}
 		}
 		return new ChangeSize(result);
