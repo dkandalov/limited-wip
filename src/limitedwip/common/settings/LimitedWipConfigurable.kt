d2c885a72badbbf1d1ca4dff01ceb9a7a89b4637
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-10-15 23:43:25 +0100

    not-nullable SettingsForm in LimitedWipConfigurable

--- src/limitedwip/common/settings/LimitedWipConfigurable.kt
+++ src/limitedwip/common/settings/LimitedWipConfigurable.kt
@@ -7,43 +7,34 @@ import com.intellij.openapi.options.SearchableConfigurable
 import com.intellij.openapi.util.Disposer
 import limitedwip.common.pluginDisplayName
 import limitedwip.common.pluginId
-import org.jetbrains.annotations.Nls
 import javax.swing.JComponent
 
 class LimitedWipConfigurable : SearchableConfigurable {
-    private var settingsForm: SettingsForm? = null
+    private lateinit var settingsForm: SettingsForm
 
     override fun createComponent(): JComponent? {
         val settings = ServiceManager.getService(LimitedWipSettings::class.java)
         settingsForm = SettingsForm(settings)
-        return settingsForm!!.root
+        return settingsForm.root
     }
 
     override fun apply() {
-        val newSettings = settingsForm!!.applyChanges()
+        val newSettings = settingsForm.applyChanges()
         Extensions.getRootArea().getExtensionPoint<Listener>(extensionPointName).extensions.forEach { listener ->
             listener.onSettingsUpdate(newSettings)
         }
     }
 
     override fun reset() {
-        settingsForm!!.resetChanges()
-        settingsForm!!.updateUIFromState()
+        settingsForm.resetChanges()
+        settingsForm.updateUIFromState()
     }
 
-    override fun disposeUIResources() {
-        settingsForm = null
-    }
+    override fun isModified() = settingsForm.isModified
 
-    override fun isModified() = settingsForm != null && settingsForm!!.isModified
-
-    @Nls override fun getDisplayName() = pluginDisplayName
+    override fun getDisplayName() = pluginDisplayName
 
     override fun getId(): String = pluginDisplayName
-
-    override fun enableSearch(option: String?): Runnable? = null
-
-    override fun getHelpTopic(): String? = null
 
     interface Listener {
         fun onSettingsUpdate(settings: LimitedWipSettings)
