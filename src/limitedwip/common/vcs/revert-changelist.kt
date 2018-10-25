2dabccbad6ad731b777be1bb47cf554c0308ac66
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2018-10-25 22:11:48 +0100

    use TransactionGuard and FileDocumentManager.reloadFiles() to revert changes

--- src/limitedwip/common/vcs/revert-changelist.kt
+++ src/limitedwip/common/vcs/revert-changelist.kt
@@ -1,38 +1,35 @@
 package limitedwip.common.vcs
 
-import com.intellij.openapi.application.ApplicationManager
-import com.intellij.openapi.application.ModalityState
+import com.intellij.openapi.application.TransactionGuard
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.vcs.changes.Change
 import com.intellij.openapi.vcs.changes.ChangeListManager
 import com.intellij.openapi.vcs.changes.ui.RollbackWorker
 import limitedwip.common.pluginId
-import java.util.concurrent.atomic.AtomicInteger
 
 private val logger = Logger.getInstance(pluginId)
 
 fun revertCurrentChangeList(project: Project): Int {
-    val revertedFilesCount = AtomicInteger(0)
-    val application = ApplicationManager.getApplication()
-    application.invokeAndWait({
-        application.runWriteAction {
-            revertedFilesCount.set(doRevert(project))
-        }
-    }, ModalityState.NON_MODAL)
-    return revertedFilesCount.get()
+    val changes = ChangeListManager.getInstance(project).defaultChangeList.changes
+    TransactionGuard.getInstance().submitTransactionLater(project, Runnable {
+        doRevert(project, changes)
+    })
+    return changes.size
 }
 
-private fun doRevert(project: Project): Int {
-    return try {
-        val changes = ChangeListManager.getInstance(project).defaultChangeList.changes
+private fun doRevert(project: Project, changes: Collection<Change>) {
+    try {
         if (changes.isNotEmpty()) {
+            // Reload files to prevent MemoryDiskConflictResolver showing with "File Cache Conflict" dialog.
+            FileDocumentManager.getInstance().reloadFiles(*changes.mapNotNull { it.virtualFile }.toTypedArray())
+
             val operationName = "$pluginId auto-revert"
             RollbackWorker(project, operationName, false).doRollback(changes, true, null, operationName)
         }
-        changes.size
     } catch (e: Exception) {
         // observed exception while reloading project at the time of auto-revert
         logger.error("Error while doing revert", e)
-        0
     }
 }
