5c1cb699b67cd19ce645ffcc2d267bdcf2d36692
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-02-07 00:01:18 +0000

    QuickCommitAction using last committed message without modifications

--- src/limitedwip/autorevert/ui/QuickCommitAction.kt
+++ src/limitedwip/autorevert/ui/QuickCommitAction.kt
@@ -18,21 +18,16 @@ import com.intellij.openapi.vcs.checkin.CheckinHandler
 import com.intellij.openapi.vcs.impl.CheckinHandlersManager
 import com.intellij.util.FunctionUtil
 import java.util.*
-import java.util.regex.Pattern
 
 
 class QuickCommitAction : AnAction(AllIcons.Actions.Commit) {
 
     override fun actionPerformed(event: AnActionEvent) {
         val project = AnAction.getEventProject(event) ?: return
-
         if (anySystemCheckinHandlerCancelsCommit(project)) return
 
         val runnable = Runnable {
             RefreshAction.doRefresh(project)
-
-            val lastCommitMessage = VcsConfiguration.getInstance(project).LAST_COMMIT_MESSAGE
-            val commitMessage = nextCommitMessage(lastCommitMessage)
 
             val defaultChangeList = ChangeListManager.getInstance(project).defaultChangeList
             if (defaultChangeList.changes.isEmpty()) {
@@ -43,24 +38,21 @@ class QuickCommitAction : AnAction(AllIcons.Actions.Commit) {
                 )
                 return@Runnable
             }
+            val lastCommitMessage = VcsConfiguration.getInstance(project).LAST_COMMIT_MESSAGE
 
-            val noCheckinHandlers = ArrayList<CheckinHandler>()
             val commitHelper = CommitHelper(
                 project,
                 defaultChangeList,
-                ArrayList(defaultChangeList.changes),
+                defaultChangeList.changes.toList(),
                 "",
-                commitMessage,
-                noCheckinHandlers, true, true, FunctionUtil.nullConstant(),
-                object : CommitResultHandler {
-                    override fun onSuccess(commitMessage: String) {}
-                    override fun onFailure() {}
-                }
+                lastCommitMessage,
+                emptyCheckinHandlers, true, true, FunctionUtil.nullConstant(),
+                noopCommitHandler
             )
 
             val committed = commitHelper.doCommit()
             if (committed) {
-                VcsConfiguration.getInstance(project).saveCommitMessage(commitMessage)
+                VcsConfiguration.getInstance(project).saveCommitMessage(lastCommitMessage)
             }
         }
 
@@ -73,8 +65,11 @@ class QuickCommitAction : AnAction(AllIcons.Actions.Commit) {
     }
 
     companion object {
-
-        private val messagePattern = Pattern.compile("^(.*?)(\\d+)$")
+        private val emptyCheckinHandlers = emptyList<CheckinHandler>()
+        private val noopCommitHandler = object: CommitResultHandler {
+            override fun onSuccess(commitMessage: String) {}
+            override fun onFailure() {}
+        }
 
         /**
          * Couldn't find better way to "reuse" this code but to copy-paste it from
@@ -82,31 +77,10 @@ class QuickCommitAction : AnAction(AllIcons.Actions.Commit) {
          */
         private fun anySystemCheckinHandlerCancelsCommit(project: Project): Boolean {
             val allActiveVcss = ProjectLevelVcsManager.getInstance(project).allActiveVcss
-            val factoryList = CheckinHandlersManager.getInstance().getRegisteredCheckinHandlerFactories(allActiveVcss)
-            return factoryList
+            return CheckinHandlersManager.getInstance()
+                .getRegisteredCheckinHandlerFactories(allActiveVcss)
                 .map { it.createSystemReadyHandler(project) }
                 .any { it != null && !it.beforeCommitDialogShown(project, ArrayList(), ArrayList(), false) }
-        }
-
-        fun nextCommitMessage(lastCommitMessage: String?): String {
-            val message = lastCommitMessage ?: ""
-            return if (message.endsWithDigits()) {
-                message.removeTailDigits() + (message.extractTailDigits().toInt() + 1)
-            } else {
-                "$message 0"
-            }
-        }
-
-        private fun String.endsWithDigits() = messagePattern.matcher(this).matches()
-
-        private fun String.extractTailDigits(): String {
-            val matcher = messagePattern.matcher(this)
-            return if (matcher.matches()) matcher.group(2) else throw IllegalStateException()
-        }
-
-        private fun String.removeTailDigits(): String {
-            val matcher = messagePattern.matcher(this)
-            return if (matcher.matches()) matcher.group(1) else throw IllegalStateException()
         }
     }
 }
