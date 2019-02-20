25ae810917ea0e768782dfd74de4044c0ec04d1f
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-02-20 13:04:32 +0000

    RollbackListener workaround

--- src/limitedwip/autorevert/components/AutorevertComponent.kt
+++ src/limitedwip/autorevert/components/AutorevertComponent.kt
@@ -3,17 +3,10 @@
 
 package limitedwip.autorevert.components
 
-import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
-import com.intellij.openapi.actionSystem.DataContext
-import com.intellij.openapi.actionSystem.ex.AnActionListener
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.ModalityState
 import com.intellij.openapi.components.AbstractProjectComponent
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.vcs.changes.ChangeListManager
-import com.intellij.openapi.vcs.changes.InvokeAfterUpdateMode
 import limitedwip.autorevert.AutoRevert
 import limitedwip.autorevert.ui.AutoRevertStatusBarWidget
 import limitedwip.common.TimerAppComponent
@@ -48,9 +41,9 @@ class AutoRevertComponent(project: Project) : AbstractProjectComponent(project) 
             }
         })
 
-        registerRollbackListener(myProject, onRollback = { allChangesRolledBack ->
+        RollbackListener(myProject) { allChangesRolledBack ->
             if (allChangesRolledBack) autoRevert.onAllChangesRolledBack()
-        })
+        }.init()
     }
 
     fun startAutoRevert() {
@@ -67,24 +60,4 @@ class AutoRevertComponent(project: Project) : AbstractProjectComponent(project) 
             minutesTillRevert.toSeconds(),
             notifyOnRevert
         )
-}
-
-private fun registerRollbackListener(project: Project, onRollback: (allChangesRolledBack: Boolean) -> Unit) {
-    val changeListManager = ChangeListManager.getInstance(project)
-    ActionManager.getInstance().addAnActionListener(object : AnActionListener {
-        override fun beforeActionPerformed(action: AnAction, dataContext: DataContext, event: AnActionEvent) {}
-
-        override fun afterActionPerformed(action: AnAction, dataContext: DataContext, event: AnActionEvent) {
-            if (action.javaClass.simpleName != "RollbackAction") return
-
-            // Note that checking changelist size immediately after rollback action will show changes as they were before the rollback
-            // (even if the check is scheduled to be run later on EDT).
-            // The following seems to be the only reliable way to do it.
-            val afterUpdate = {
-                val changes = changeListManager.defaultChangeList.changes
-                onRollback(changes.isEmpty())
-            }
-            changeListManager.invokeAfterUpdate(afterUpdate, InvokeAfterUpdateMode.SILENT_CALLBACK_POOLED, null, ModalityState.any())
-        }
-    }, project)
 }
---
+++ src/limitedwip/autorevert/components/RollbackListener.java
@@ -0,0 +1,55 @@
+package limitedwip.autorevert.components;
+
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.actionSystem.ex.AnActionListener;
+import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.changes.Change;
+import com.intellij.openapi.vcs.changes.ChangeListManager;
+import com.intellij.openapi.vcs.changes.InvokeAfterUpdateMode;
+import kotlin.Unit;
+
+import java.util.Collection;
+import java.util.function.Function;
+
+/**
+ * This class is written in Java because in the latest version IJ API forces not nullable AnActionEvents in callbacks.
+ * However, they can actually be null in older versions.
+ * I.e. API changes force you to compile code which doesn't work in older versions of IJ.
+ */
+public class RollbackListener {
+    private final Project project;
+    private final Function<Boolean, Unit> onRollback;
+
+    public RollbackListener(Project project, Function<Boolean, Unit> onRollback) {
+        this.project = project;
+        this.onRollback = onRollback;
+    }
+
+    public void init() {
+        ChangeListManager changeListManager = ChangeListManager.getInstance(project);
+        ActionManager.getInstance().addAnActionListener(
+                new AnActionListener() {
+                    @Override public void beforeActionPerformed(AnAction action, DataContext dataContext, AnActionEvent event) {
+                    }
+
+                    @Override public void afterActionPerformed(AnAction action, DataContext dataContext, AnActionEvent event) {
+                        if (event == null) return;
+                        if (!action.getClass().getSimpleName().equals("RollbackAction")) return;
+
+                        // Note that checking changelist size immediately after rollback action will show changes as they were before the rollback
+                        // (even if the check is scheduled to be run later on EDT).
+                        // The following seems to be the only reliable way to do it.
+                        Runnable afterUpdate = () -> {
+                            Collection<Change> changes = changeListManager.getDefaultChangeList().getChanges();
+                            onRollback.apply(changes.isEmpty());
+                        };
+                        changeListManager.invokeAfterUpdate(afterUpdate, InvokeAfterUpdateMode.SILENT_CALLBACK_POOLED, null, ModalityState.any());
+                    }
+                }
+        );
+    }
+}
