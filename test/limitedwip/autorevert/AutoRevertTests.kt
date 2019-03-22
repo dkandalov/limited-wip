d5ed9b2c3e5971b5e4d9811b1262855a3a9b019c
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-03-22 09:46:08 +0200

    cleanup

--- test/limitedwip/autorevert/AutoRevertTests.kt
+++ test/limitedwip/autorevert/AutoRevertTests.kt
@@ -21,15 +21,8 @@ class AutoRevertTests {
     private val autoRevert = AutoRevert(ide, settings)
     private var seconds: Int = 0
 
-    @Test fun `show time till revert when started`() {
+    @Test fun `automatically start timer when some changes are made`() {
         autoRevert.onNextTimer()
-        ide.expect(inOrder, times(0)).showTimeTillRevert(anyInt())
-
-        autoRevert.onTimerWithChanges()
-        ide.expect(inOrder, times(1)).showTimeTillRevert(anyInt())
-    }
-
-    @Test fun `automatically start on timer update with some changes`() {
         autoRevert.onNextTimer()
         ide.expect(inOrder, times(0)).showTimeTillRevert(anyInt())
 
@@ -44,47 +37,6 @@ class AutoRevertTests {
         ide.expect(inOrder).notifyThatChangesWereReverted()
     }
 
-    @Test fun `don't revert changes while commit dialog is open`() {
-        autoRevert.onTimerWithChanges()
-        whenCalled(ide.isCommitDialogOpen()).thenReturn(true)
-        autoRevert.onTimerWithChanges()
-        autoRevert.onTimerWithChanges()
-
-        ide.expect(never()).revertCurrentChangeList()
-        ide.expect(never()).notifyThatChangesWereReverted()
-    }
-
-    @Test fun `revert changes after commit dialog is closed`() {
-        autoRevert.onTimerWithChanges()
-        whenCalled(ide.isCommitDialogOpen()).thenReturn(true)
-        autoRevert.onTimerWithChanges()
-
-        whenCalled(ide.isCommitDialogOpen()).thenReturn(false)
-        autoRevert.onTimerWithChanges()
-        ide.expect().revertCurrentChangeList()
-        ide.expect().notifyThatChangesWereReverted()
-    }
-
-    @Test fun `don't start timer after timeout with commit dialog open`() {
-        autoRevert.onTimerWithChanges()
-        ide.expect(inOrder, times(1)).showTimeTillRevert(eq(2))
-
-        whenCalled(ide.isCommitDialogOpen()).thenReturn(true)
-        autoRevert.onNextTimer()
-        ide.expect(inOrder).showTimeTillRevert(eq(1))
-        autoRevert.onNextTimer()
-        ide.expect(inOrder).showTimeTillRevert(eq(2))
-        autoRevert.onNextTimer()
-        ide.expect(inOrder).showTimeTillRevert(eq(2)) // timer is still 2
-
-        whenCalled(ide.isCommitDialogOpen()).thenReturn(false)
-        autoRevert.onNextTimer()
-        autoRevert.onNextTimer()
-        ide.expect(inOrder).showTimeTillRevert(eq(2))
-        autoRevert.onNextTimer()
-        ide.expect(inOrder).showTimeTillRevert(eq(1))
-    }
-
     @Test fun `reset timer on commit`() {
         autoRevert.onTimerWithChanges()
         ide.expect(inOrder, times(1)).showTimeTillRevert(eq(2))
@@ -97,7 +49,49 @@ class AutoRevertTests {
         ide.expect(inOrder).showTimeTillRevert(eq(1))
     }
 
-    @Test fun `use updated settings`() {
+    @Test fun `when commit dialog is open, don't revert changes`() {
+        autoRevert.onTimerWithChanges()
+        whenCalled(ide.isCommitDialogOpen()).thenReturn(true)
+        autoRevert.onTimerWithChanges()
+        autoRevert.onTimerWithChanges()
+        autoRevert.onTimerWithChanges()
+
+        ide.expect(never()).revertCurrentChangeList()
+        ide.expect(never()).notifyThatChangesWereReverted()
+    }
+
+    @Test fun `after commit dialog is closed, revert pending changes`() {
+        autoRevert.onTimerWithChanges()
+        whenCalled(ide.isCommitDialogOpen()).thenReturn(true)
+        autoRevert.onTimerWithChanges()
+
+        whenCalled(ide.isCommitDialogOpen()).thenReturn(false)
+        autoRevert.onTimerWithChanges()
+        ide.expect().revertCurrentChangeList()
+        ide.expect().notifyThatChangesWereReverted()
+    }
+
+    @Test fun `when commit dialog is open, don't start timer after timeout`() {
+        autoRevert.onTimerWithChanges()
+        ide.expect(inOrder, times(1)).showTimeTillRevert(eq(2))
+
+        whenCalled(ide.isCommitDialogOpen()).thenReturn(true)
+        autoRevert.onTimerWithChanges()
+        ide.expect(inOrder).showTimeTillRevert(eq(1))
+        autoRevert.onTimerWithChanges()
+        ide.expect(inOrder).showTimeTillRevert(eq(2))
+        autoRevert.onTimerWithChanges()
+        ide.expect(inOrder).showTimeTillRevert(eq(2)) // timer is still 2
+
+        whenCalled(ide.isCommitDialogOpen()).thenReturn(false)
+        autoRevert.onTimerWithChanges()
+        autoRevert.onTimerWithChanges()
+        ide.expect(inOrder).showTimeTillRevert(eq(2))
+        autoRevert.onTimerWithChanges()
+        ide.expect(inOrder).showTimeTillRevert(eq(1))
+    }
+
+    @Test fun `use updated 'secondsTillRevert' settings`() {
         autoRevert.onSettingsUpdate(settings.copy(secondsTillRevert = 1))
         autoRevert.onTimerWithChanges()
         autoRevert.onTimerWithChanges()
@@ -106,7 +100,7 @@ class AutoRevertTests {
         ide.expect(times(2)).notifyThatChangesWereReverted()
     }
 
-    @Test fun `use updated settings after the end of the current timeout`() {
+    @Test fun `use updated 'secondsTillRevert' settings after the end of the current timeout`() {
         autoRevert.onTimerWithChanges()
         autoRevert.onSettingsUpdate(settings.copy(secondsTillRevert = 1)) // settings not applied yet
         autoRevert.onTimerWithChanges() // reverts changes after 2nd time event; settings applied
@@ -117,7 +111,7 @@ class AutoRevertTests {
         ide.expect(times(3)).notifyThatChangesWereReverted()
     }
 
-    @Test fun `use updated settings after commit`() {
+    @Test fun `use updated 'secondsTillRevert' settings after commit`() {
         autoRevert.onTimerWithChanges()
         autoRevert.onSettingsUpdate(settings.copy(secondsTillRevert = 1)) // settings not applied yet
         autoRevert.onAllChangesCommitted() // settings applied
@@ -129,8 +123,8 @@ class AutoRevertTests {
         ide.expect(times(3)).notifyThatChangesWereReverted()
     }
 
-    @Test fun `don't show anything in UI when disabled`() {
-        val disabledSettings = Settings(false, secondsTillRevert, false)
+    @Test fun `when disabled, don't show anything in UI`() {
+        val disabledSettings = settings.copy(autoRevertEnabled = false)
         autoRevert.onSettingsUpdate(disabledSettings)
         autoRevert.onTimerWithChanges()
 
@@ -139,9 +133,10 @@ class AutoRevertTests {
         ide.expectNoMoreInteractions()
     }
 
-    @Test fun `don't revert changes when disabled`() {
+    @Test fun `when disabled while timer is running, don't revert changes`() {
+        val disabledSettings = settings.copy(autoRevertEnabled = false)
         autoRevert.onTimerWithChanges()
-        autoRevert.onSettingsUpdate(Settings(false, 2, false))
+        autoRevert.onSettingsUpdate(disabledSettings)
         autoRevert.onTimerWithChanges()
 
         ide.expect(never()).revertCurrentChangeList()
