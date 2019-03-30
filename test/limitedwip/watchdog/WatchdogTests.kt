190c9f73df29248d696260ef08d79368411ac61a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-03-30 13:41:10 +0000

    refactoring tests to use explicit fixture

--- test/limitedwip/watchdog/WatchdogTests.kt
+++ test/limitedwip/watchdog/WatchdogTests.kt
@@ -11,24 +11,59 @@ import org.mockito.internal.matchers.InstanceOf
 import org.mockito.internal.progress.ThreadSafeMockingProgress.mockingProgress
 import org.mockito.Mockito.`when` as whenCalled
 
-class WatchdogTests {
-    private val maxLinesInChange = 100
-    private val settings = Settings(
+data class Fixture(
+    val maxLinesInChange: Int = 100,
+    val settings: Settings = Settings(
         enabled = true,
         maxLinesInChange = maxLinesInChange,
         notificationIntervalInSeconds = 2,
         showRemainingChangesInToolbar = true,
-        noCommitsAboveThreshold = false,
+        noCommitsAboveThreshold = true,
         exclusions = setOf(
             PathMatcher.parse("some/excluded/path"),
             PathMatcher.parse("another/excluded/path")
         )
-    )
-    private val disabledSettings = settings.copy(enabled = false)
-    private val ide = mock(Ide::class.java)
-    private val watchdog = Watchdog(ide, settings)
+    ),
+    val disabledSettings: Settings = settings.copy(enabled = false),
+    val ide: Ide = mock(Ide::class.java),
+    val watchdog: Watchdog = Watchdog(ide, settings)
+) {
+    fun someChangesWithSize(size: Int) =
+        ChangeSizesWithPath(listOf(Pair("", ChangeSize(size))))
 
-    @Test fun `don't send notification when change size is below threshold`() {
+    fun anySettings(): Watchdog.Settings {
+        val type = Watchdog.Settings::class.java
+        mockingProgress().argumentMatcherStorage.reportMatcher(InstanceOf.VarArgAware(type, "<any ${type.canonicalName}>"))
+        return Watchdog.Settings(false, 0, 0, false, false, emptySet())
+    }
+
+    fun anyChangeSize(): ChangeSize {
+        val type = ChangeSize::class.java
+        mockingProgress().argumentMatcherStorage.reportMatcher(InstanceOf.VarArgAware(type, "<any ${type.canonicalName}>"))
+        return ChangeSize(0, false)
+    }
+}
+
+class WatchdogTests {
+
+    @Test fun `show in UI current change size`() = Fixture().run {
+        whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(200))
+        watchdog.onTimer()
+        ide.expect().showCurrentChangeListSize(ChangeSize(200), maxLinesInChange)
+    }
+
+    @Test fun `send notification when change size is above threshold`() = Fixture().run {
+        whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(200))
+
+        watchdog.onTimer() // send notification
+        watchdog.onTimer()
+        watchdog.onTimer() // send notification
+        watchdog.onTimer()
+
+        ide.expect(times(2)).showNotificationThatChangeSizeIsTooBig(ChangeSize(200), maxLinesInChange)
+    }
+
+    @Test fun `don't send notification when change size is below threshold`() = Fixture().run {
         whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(10))
 
         watchdog.onTimer()
@@ -36,7 +71,8 @@ class WatchdogTests {
         ide.expect(never()).showNotificationThatChangeSizeIsTooBig(anyChangeSize(), anyInt())
     }
 
-    @Test fun `don't send notification when change size has excluded paths which make it below threshold`() {
+    @Test
+    fun `don't send notification when change size has excluded paths which make it below threshold`() = Fixture().run {
         whenCalled(ide.currentChangeListSizeInLines()).thenReturn(ChangeSizesWithPath(listOf(
             Pair("/some/path", ChangeSize(90)),
             Pair("/some/excluded/path", ChangeSize(200)),
@@ -48,26 +84,7 @@ class WatchdogTests {
         ide.expect(never()).showNotificationThatChangeSizeIsTooBig(anyChangeSize(), anyInt())
     }
 
-    @Test fun `send notification when change size is above threshold`() {
-        whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(200))
-
-        watchdog.onTimer()
-
-        ide.expect().showNotificationThatChangeSizeIsTooBig(ChangeSize(200), maxLinesInChange)
-    }
-
-    @Test fun `send change size notification only on one of several updates`() {
-        whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(200))
-
-        watchdog.onTimer() // send notification
-        watchdog.onTimer()
-        watchdog.onTimer() // send notification
-        watchdog.onTimer()
-
-        ide.expect(times(2)).showNotificationThatChangeSizeIsTooBig(ChangeSize(200), maxLinesInChange)
-    }
-
-    @Test fun `send change size notification after settings change`() {
+    @Test fun `send change size notification after settings change`() = Fixture().run {
         whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(200))
         val inOrder = inOrder(ide)
 
@@ -81,18 +98,7 @@ class WatchdogTests {
         ide.expect(inOrder).showNotificationThatChangeSizeIsTooBig(ChangeSize(200), 150)
     }
 
-    @Test fun `don't send notification when disabled`() {
-        whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(200))
-
-        watchdog.onSettingsUpdate(disabledSettings)
-        watchdog.onTimer()
-        watchdog.onTimer()
-
-        ide.expect(times(2)).onSettingsUpdate(anySettings())
-        ide.expectNoMoreInteractions()
-    }
-
-    @Test fun `skip notifications util next commit`() {
+    @Test fun `skip notifications util next commit`() = Fixture().run {
         whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(200))
 
         watchdog.onSkipNotificationsUntilCommit()
@@ -104,13 +110,7 @@ class WatchdogTests {
         ide.expect().showNotificationThatChangeSizeIsTooBig(ChangeSize(200), maxLinesInChange)
     }
 
-    @Test fun `send change size update`() {
-        whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(200))
-        watchdog.onTimer()
-        ide.expect().showCurrentChangeListSize(ChangeSize(200), maxLinesInChange)
-    }
-
-    @Test fun `still send change size update when notifications are skipped till next commit`() {
+    @Test fun `still send change size update when notifications are skipped till next commit`() = Fixture().run {
         whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(200))
 
         watchdog.onSkipNotificationsUntilCommit()
@@ -119,7 +119,7 @@ class WatchdogTests {
         ide.expect().showCurrentChangeListSize(ChangeSize(200), maxLinesInChange)
     }
 
-    @Test fun `don't send change size update when disabled`() {
+    @Test fun `don't send change size update when disabled`() = Fixture().run {
         whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(200))
 
         watchdog.onSettingsUpdate(disabledSettings)
@@ -129,7 +129,18 @@ class WatchdogTests {
         ide.expectNoMoreInteractions()
     }
 
-    @Test fun `close notification when change size is back within limit`() {
+    @Test fun `don't send notification when disabled`() = Fixture().run {
+        whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(200))
+
+        watchdog.onSettingsUpdate(disabledSettings)
+        watchdog.onTimer()
+        watchdog.onTimer()
+
+        ide.expect(times(2)).onSettingsUpdate(anySettings())
+        ide.expectNoMoreInteractions()
+    }
+
+    @Test fun `close notification when change size is back within limit`() = Fixture().run {
         whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(200))
         watchdog.onTimer()
         whenCalled(ide.currentChangeListSizeInLines()).thenReturn(someChangesWithSize(0))
@@ -140,35 +151,16 @@ class WatchdogTests {
         ide.expect(times(2)).hideNotificationThatChangeSizeIsTooBig()
     }
 
-    @Test fun `don't allow commits above threshold`() {
-        watchdog.onSettingsUpdate(settings.copy(noCommitsAboveThreshold = true))
-
+    @Test fun `don't allow commits above threshold`() = Fixture().run {
         watchdog.isCommitAllowed(someChangesWithSize(200)) shouldEqual false
         ide.expect().notifyThatCommitWasCancelled()
     }
 
-    @Test fun `allow one commit above threshold when forced`() {
-        watchdog.onSettingsUpdate(settings.copy(noCommitsAboveThreshold = true))
-
+    @Test fun `allow one commit above threshold when forced`() = Fixture().run {
         watchdog.isCommitAllowed(someChangesWithSize(200)) shouldEqual false
         ide.expect().notifyThatCommitWasCancelled()
 
         watchdog.onForceCommit()
         watchdog.isCommitAllowed(someChangesWithSize(200)) shouldEqual true
-    }
-
-    private fun someChangesWithSize(size: Int) =
-        ChangeSizesWithPath(listOf(Pair("", ChangeSize(size))))
-
-    private fun anySettings(): Watchdog.Settings {
-        val type = Watchdog.Settings::class.java
-        mockingProgress().argumentMatcherStorage.reportMatcher(InstanceOf.VarArgAware(type, "<any ${type.canonicalName}>"))
-        return Watchdog.Settings(false, 0, 0, false, false, emptySet())
-    }
-
-    private fun anyChangeSize(): ChangeSize {
-        val type = ChangeSize::class.java
-        mockingProgress().argumentMatcherStorage.reportMatcher(InstanceOf.VarArgAware(type, "<any ${type.canonicalName}>"))
-        return ChangeSize(0, false)
     }
 }
