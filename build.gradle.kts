a1cfbbc4cff9a42666b952cdf934dc6404790d0a
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-06-26 09:20:37 +0100

    misc

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,10 @@ tasks.withType<KotlinJvmCompile> {
 
 configure<IntelliJPluginExtension> {
     // See https://www.jetbrains.com/intellij-repository/releases for a list of available IDEA builds
-    val ideVersion = System.getenv().getOrDefault("LIMITED_WIP_PLUGIN_IDEA_VERSION", "IC-173.2099.4")
+    val ideVersion = System.getenv().getOrDefault("LIMITED_WIP_PLUGIN_IDEA_VERSION",
+        "IC-173.2099.4"
+//        "LATEST-EAP-SNAPSHOT"
+    )
     println("Using ide version: $ideVersion")
     version = ideVersion
     pluginName = "LimitedWIP"
