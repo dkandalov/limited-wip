1cefbb22b352e0d03ce380ca0a1d4728acdbecb2
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2019-07-30 23:57:53 +0100

    minimum IJ 182.2949.2 to fix build

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,7 +55,7 @@ tasks.withType<KotlinJvmCompile> {
 configure<IntelliJPluginExtension> {
     // See https://www.jetbrains.com/intellij-repository/releases for a list of available IDEA builds
     val ideVersion = System.getenv().getOrDefault("LIMITED_WIP_PLUGIN_IDEA_VERSION",
-        "IC-181.3870.7"
+        "IC-182.2949.2"
 //        "LATEST-EAP-SNAPSHOT"
     )
     println("Using ide version: $ideVersion")
