54346398039af725de6e921a21afbef60a8e1823
Dmitry Kandalov <dmitry.kandalov@gmail.com>
2021-02-14 20:34:48 +0000

    store settings in .idea/limited-wip.xml (issue #34)

--- src/limitedwip/common/settings/LimitedWipSettings.kt
+++ src/limitedwip/common/settings/LimitedWipSettings.kt
@@ -4,16 +4,16 @@ import com.intellij.openapi.Disposable
 import com.intellij.openapi.components.PersistentStateComponent
 import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.util.xmlb.XmlSerializerUtil
-import limitedwip.common.pluginId
 import limitedwip.common.settings.CommitMessageSource.LastCommit
 import limitedwip.common.settings.LimitedWipSettings.Companion.never
 import limitedwip.common.settings.TcrAction.Commit
 import limitedwip.common.settings.TimeUnit.Minutes
 
-@State(name = "${pluginId}Settings")
+@State(name = "LimitedWIPPluginSettings", storages = [Storage(value = "limited-wip.xml")])
 data class LimitedWipSettings(
     var watchdogEnabled: Boolean = true,
     var maxLinesInChange: Int = 80,
@@ -23,8 +23,6 @@ data class LimitedWipSettings(
     var exclusions: String = "",
 
     var autoRevertEnabled: Boolean = false,
-    @Deprecated("Should be removed in the future when it seems like all the users have upgraded")
-    var minutesTillRevert: Int = never,
     var timeTillRevert: Int = 2,
     var timeUnitTillRevert: TimeUnit = Minutes,
     var notifyOnRevert: Boolean = true,
@@ -35,20 +33,17 @@ data class LimitedWipSettings(
     var commitMessageSource: CommitMessageSource = LastCommit,
     var notifyOnTcrRevert: Boolean = false,
     var doNotRevertTests: Boolean = false,
-    var doNotRevertFiles: String = ""
+    var doNotRevertFiles: String = "",
+
+    var migratedFromOldLocation: Boolean = false
 ): PersistentStateComponent<LimitedWipSettings> {
     private val listeners = ArrayList<Listener>()
 
-    override fun getState(): LimitedWipSettings = this
+    override fun getState() = this
 
     @Suppress("DEPRECATION")
     override fun loadState(state: LimitedWipSettings) {
         XmlSerializerUtil.copyBean(state, this)
-        if (minutesTillRevert != never) {
-            timeTillRevert = minutesTillRevert
-            timeUnitTillRevert = Minutes
-            minutesTillRevert = never
-        }
         notifyListeners()
     }
 
@@ -75,8 +70,104 @@ data class LimitedWipSettings(
         fun isValidChangedSizeRange(lineCount: Int) = lineCount in changedLinesRange
         fun isValidNotificationInterval(interval: Int) = interval in notificationIntervalRange
 
-        fun getInstance(project: Project): LimitedWipSettings =
-            ServiceManager.getService(project, LimitedWipSettings::class.java)
+        fun getInstance(project: Project): LimitedWipSettings {
+            val settings = ServiceManager.getService(project, LimitedWipSettings::class.java)
+            return if (settings.migratedFromOldLocation) settings
+            else settings.also {
+                val oldSettings = OldLocation_LimitedWipSettings.getInstance(project)
+                it.watchdogEnabled = oldSettings.watchdogEnabled
+                it.maxLinesInChange = oldSettings.maxLinesInChange
+                it.notificationIntervalInMinutes = oldSettings.notificationIntervalInMinutes
+                it.noCommitsAboveThreshold = oldSettings.noCommitsAboveThreshold
+                it.showRemainingChangesInToolbar = oldSettings.showRemainingChangesInToolbar
+                it.exclusions = oldSettings.exclusions
+
+                it.autoRevertEnabled = oldSettings.autoRevertEnabled
+                it.timeTillRevert = oldSettings.timeTillRevert
+                it.timeUnitTillRevert = oldSettings.timeUnitTillRevert
+                it.notifyOnRevert = oldSettings.notifyOnRevert
+                it.showTimerInToolbar = oldSettings.showTimerInToolbar
+
+                it.tcrEnabled = oldSettings.tcrEnabled
+                it.tcrActionOnPassedTest = oldSettings.tcrActionOnPassedTest
+                it.commitMessageSource = oldSettings.commitMessageSource
+                it.notifyOnTcrRevert = oldSettings.notifyOnTcrRevert
+                it.doNotRevertTests = oldSettings.doNotRevertTests
+                it.doNotRevertFiles = oldSettings.doNotRevertFiles
+
+                it.migratedFromOldLocation = true
+                // Reset to defaults so that settings are removed from the misc.xml
+                oldSettings.resetToDefaults()
+            }
+        }
+    }
+}
+
+@Suppress("ClassName")
+@State(name = "LimitedWIPSettings") // Was stored in .idea/misc.xml
+data class OldLocation_LimitedWipSettings(
+    var watchdogEnabled: Boolean = true,
+    var maxLinesInChange: Int = 80,
+    var notificationIntervalInMinutes: Int = never,
+    var noCommitsAboveThreshold: Boolean = false,
+    var showRemainingChangesInToolbar: Boolean = true,
+    var exclusions: String = "",
+
+    var autoRevertEnabled: Boolean = false,
+    @Deprecated("Should be removed in the future when it seems like all the users have upgraded")
+    var minutesTillRevert: Int = never,
+    var timeTillRevert: Int = 2,
+    var timeUnitTillRevert: TimeUnit = Minutes,
+    var notifyOnRevert: Boolean = true,
+    var showTimerInToolbar: Boolean = true,
+
+    var tcrEnabled: Boolean = false,
+    var tcrActionOnPassedTest: TcrAction = Commit,
+    var commitMessageSource: CommitMessageSource = LastCommit,
+    var notifyOnTcrRevert: Boolean = false,
+    var doNotRevertTests: Boolean = false,
+    var doNotRevertFiles: String = ""
+): PersistentStateComponent<OldLocation_LimitedWipSettings> {
+
+    override fun getState() = this
+
+    @Suppress("DEPRECATION")
+    override fun loadState(state: OldLocation_LimitedWipSettings) {
+        XmlSerializerUtil.copyBean(state, this)
+        if (minutesTillRevert != never) {
+            timeTillRevert = minutesTillRevert
+            timeUnitTillRevert = Minutes
+            minutesTillRevert = never
+        }
+    }
+
+    fun resetToDefaults() {
+        watchdogEnabled = true
+        maxLinesInChange = 80
+        notificationIntervalInMinutes = never
+        noCommitsAboveThreshold = false
+        showRemainingChangesInToolbar = true
+        exclusions = ""
+
+        autoRevertEnabled = false
+        @Suppress("DEPRECATION")
+        minutesTillRevert = never
+        timeTillRevert = 2
+        timeUnitTillRevert = Minutes
+        notifyOnRevert = true
+        showTimerInToolbar = true
+
+        tcrEnabled = false
+        tcrActionOnPassedTest = Commit
+        commitMessageSource = LastCommit
+        notifyOnTcrRevert = false
+        doNotRevertTests = false
+        doNotRevertFiles = ""
+    }
+
+    companion object {
+        fun getInstance(project: Project): OldLocation_LimitedWipSettings =
+            ServiceManager.getService(project, OldLocation_LimitedWipSettings::class.java)
     }
 }
 
